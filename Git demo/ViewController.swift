aef96c40108b74c7e69eea31dbf3749a8b7236fd
Seevan Ranka <seevan.ranka@forgeahead.io>
2020-01-31 20:32:25 +0530

    Branch hopping

--- Git demo/ViewController.swift
+++ Git demo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Demo of branch hopping in GIt")
         // Do any additional setup after loading the view.
     }
 
